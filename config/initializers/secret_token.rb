1aa0456ac2057ee88532270158a74af1be8d3af0
Shearry Malone <sleam32@gmail.com>
2013-06-12 12:08:02 -0500

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMix::Application.config.secret_token = '5e2ecf9647980b275b01ebdcb96f5dcd4d03e3c02c8a9184a2c3fbda4bffbfe47f2748b051957afc80c7361e55a00331725833c1183fad7e24e6dee5769125a1'
